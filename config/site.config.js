30b9614ce865e804cc09b2725936cde560ba5112
cuiyangeng <108133635+cuiyangeng@users.noreply.github.com>
2022-06-25 12:22:48 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
 
 
-  // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
+   quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:   
      '崔某人的分享网盘',
   
