4e14aa3d02561cee1bd40b8fe0baf4b56137f073
cuiyangeng <108133635+cuiyangeng@users.noreply.github.com>
2022-06-25 12:24:26 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,11 +35,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
 
 
-   quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
+   //quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:   
-     '崔某人的分享网盘',
+     
   
-    'Powered by <a href="/" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    'Powered by <a href="/" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.','崔某人的分享网盘',
   
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
